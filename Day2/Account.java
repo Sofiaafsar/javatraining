1d3a23b52284cb35afa904fe275fab77405727ae
Sofiaafsar <115290901+Sofiaafsar@users.noreply.github.com>
2024-02-22 22:41:30 +0530

    Update Account.java

--- Day2/Account.java
+++ Day2/Account.java
@@ -1,1 +1,45 @@
+package Inheritance;
+import java.util.*;
+class Account {
+    private String accno;
+    private int balance;
+    public Account(String accno, int balance) {
+        this.accno = accno;
+        this.balance = balance;
+    }
+
+    public String getAccno() {
+        return accno;
+    }
+
+    public void setAccno(String accno) {
+        this.accno = accno;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+    public void deposit(int depositamt){
+        balance+=depositamt;
+        System.out.println("The balance amt :"+balance);
+    }
+    public void withdraw(int withdrawamt){
+        if(balance-withdrawamt>0){
+            balance=balance-withdrawamt;
+            System.out.println("The balance amt :"+balance);
+        }
+        else{
+            System.out.println("Insufficient balance");
+        }
+    }
+}
+public class Accounts {
+ public static void main(String[]args){
+     Account acc=new Account("ABX1234",50000);
+     acc.withdraw(51000);
+     acc.deposit(1000);
+     acc.withdraw(2000);
+ }
+}
+
 
